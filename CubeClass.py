8f7d596047b5137f35d69142ef6b33be3d9e1eb9
Raymond Zou <raymond.1zou@gmail.com>
2023-08-14 14:28:11 -0700

    add turning methods for each face of the cube

--- CubeClass.py
+++ CubeClass.py
@@ -36,12 +36,13 @@ class Cube:
     def turn_U(self, turn_num):
         s = self
         # set side faces
-        temp_arr = [s.green[:3], s.orange[:3], s.blue[:3], s.red[:3]]
-        temp_arr = np.roll(temp_arr, shift=turn_num, axis=0)
-        s.green[:3] = temp_arr[0]
-        s.orange[:3] = temp_arr[1]
-        s.blue[:3] = temp_arr[2]
-        s.red[:3] = temp_arr[3]
+        temp_arr = np.concatenate(
+            (s.green[:3], s.orange[:3], s.blue[:3], s.red[:3]))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.green[:3] = temp_arr[:3]
+        s.orange[:3] = temp_arr[3:6]
+        s.blue[:3] = temp_arr[6:9]
+        s.red[:3] = temp_arr[9:12]
         # set main face corners
         corners = s.white[[0, 2, 8, 6]]
         s.white[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
@@ -49,7 +50,107 @@ class Cube:
         edges = s.white[[1, 5, 7, 3]]
         s.white[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
 
-    # def turn_D(self, turn_num):
-    #     s = self
-    #     # set side faces
-    #     temp_arr = [s.green[]]
+    def turn_D(self, turn_num):
+        s = self
+        # set side faces
+        temp_arr = np.concatenate(
+            (s.green[-3:], s.red[-3:], s.blue[-3:], s.orange[-3:]))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.green[-3:] = temp_arr[:3]
+        s.red[-3:] = temp_arr[3:6]
+        s.blue[-3:] = temp_arr[6:9]
+        s.orange[-3:] = temp_arr[9:12]
+        # set main face corners
+        corners = s.yellow[[0, 2, 8, 6]]
+        s.yellow[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
+        # set main face edges
+        edges = s.yellow[[1, 5, 7, 3]]
+        s.yellow[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
+
+    def turn_R(self, turn_num):
+        s = self
+        # set side faces
+        temp_arr = np.concatenate(
+            (s.green[[8, 5, 2]], s.white[[8, 5, 2]], s.blue[[0, 3, 6]], s.yellow[[8, 5, 2]],))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.green[[8, 5, 2]] = temp_arr[:3]
+        s.white[[8, 5, 2]] = temp_arr[3:6]
+        s.blue[[0, 3, 6]] = temp_arr[6:9]
+        s.yellow[[8, 5, 2]] = temp_arr[9:12]
+        # set main face corners
+        corners = s.red[[0, 2, 8, 6]]
+        s.red[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
+        # set main face edges
+        edges = s.red[[1, 5, 7, 3]]
+        s.red[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
+
+    def turn_L(self, turn_num):
+        s = self
+        # set side faces
+        temp_arr = np.concatenate((s.green[[0, 3, 6]],
+                                   s.yellow[[0, 3, 6]],
+                                   s.blue[[8, 5, 2]],
+                                   s.white[[0, 3, 6]],))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.green[[0, 3, 6]] = temp_arr[:3]
+        s.yellow[[0, 3, 6]] = temp_arr[3:6]
+        s.blue[[8, 5, 2]] = temp_arr[6:9]
+        s.white[[0, 3, 6]] = temp_arr[9:12]
+        # set main face corners
+        corners = s.orange[[0, 2, 8, 6]]
+        s.orange[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
+        # set main face edges
+        edges = s.orange[[1, 5, 7, 3]]
+        s.orange[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
+
+    def turn_F(self, turn_num):
+        s = self
+        temp_arr = np.concatenate(
+            (s.white[[6, 7, 8]], s.red[[0, 3, 6]], s.yellow[[2, 1, 0]], s.orange[[8, 5, 2]]))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.white[[6, 7, 8]] = temp_arr[:3]
+        s.red[[0, 3, 6]] = temp_arr[3:6]
+        s.yellow[[2, 1, 0]] = temp_arr[6:9]
+        s.orange[[8, 5, 2]] = temp_arr[9:12]
+        # set main face corners
+        corners = s.green[[0, 2, 8, 6]]
+        s.green[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
+        # set main face edges
+        edges = s.green[[1, 5, 7, 3]]
+        s.green[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
+
+    def turn_B(self, turn_num):
+        s = self
+        # set side faces
+        temp_arr = np.concatenate(
+            (s.white[[2, 1, 0]], s.orange[[0, 3, 6]], s.yellow[[6, 7, 8]], s.red[[8, 5, 2]]))
+        temp_arr = np.roll(temp_arr, shift=turn_num*3, axis=0)
+        s.white[[2, 1, 0]] = temp_arr[:3]
+        s.orange[[0, 3, 6]] = temp_arr[3:6]
+        s.yellow[[6, 7, 8]] = temp_arr[6:9]
+        s.red[[8, 5, 2]] = temp_arr[9:12]
+        # set main face corners
+        corners = s.blue[[0, 2, 8, 6]]
+        s.blue[[0, 2, 8, 6]] = np.roll(corners, shift=turn_num)
+        # set main face edges
+        edges = s.blue[[1, 5, 7, 3]]
+        s.blue[[1, 5, 7, 3]] = np.roll(edges, shift=turn_num)
+
+    def print_faces(self):
+        print('\n'.join(''.join(self.white[i:i+3])
+                        for i in range(0, len(self.white), 3)))
+        print('------------')
+        print('\n'.join(''.join(self.green[i:i+3])
+                        for i in range(0, len(self.green), 3)))
+        print('------------')
+        print('\n'.join(''.join(self.red[i:i+3])
+                        for i in range(0, len(self.red), 3)))
+        print('------------')
+        print('\n'.join(''.join(self.blue[i:i+3])
+                        for i in range(0, len(self.blue), 3)))
+        print('------------')
+        print('\n'.join(''.join(self.orange[i:i+3])
+                        for i in range(0, len(self.orange), 3)))
+        print('------------')
+        print('\n'.join(''.join(self.yellow[i:i+3])
+                        for i in range(0, len(self.yellow), 3)))
--- Solver.py
+++ Solver.py
@@ -1,10 +1,13 @@
 from CubeClass import Cube
 import numpy as np
 
-# cube = Cube()
-# cube.white = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
-# cube.turn_U(1)
-# print(cube.green)
-# print(cube.white)
-
-arr = [1, 2, 3]
+w, y, r, o, g, b = 'w', 'y', 'r', 'o', 'g', 'b'
+sample_cube = [
+    [y, r, g, o, g, g, o, w, g],
+    [o, b, b, y, b, y, b, b, g],
+    [r, b, w, r, r, g, y, o, w],
+    [o, w, r, o, o, g, r, w, b],
+    [y, g, o, b, y, w, y, r, r],
+    [w, o, g, r, w, y, b, y, w]
+]
+cube = Cube()
