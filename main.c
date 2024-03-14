5460776db24559ab41bc2527377ff91ba74ca57c
raymond <raymond.1zou@gmail.com>
2024-03-14 11:53:05 -0700

    update Cube struct

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 // arbitrarily
 #define W 0x00
@@ -13,12 +12,12 @@
 // 48 bytes for 48 stickers on the cube
 // 1 face looks like 0x 00 00 00 00 00 00 00 00 = 8 bytes = 64 bits
 struct Cube {
-  long long white_face;
-  long long orange_face;
-  long long green_face;
-  long long red_face;
-  long long blue_face;
-  long long yellow_face;
+  unsigned char white_face[8];
+  unsigned char orange_face[8];
+  unsigned char green_face[8];
+  unsigned char red_face[8];
+  unsigned char blue_face[8];
+  unsigned char yellow_face[8];
 };
 
 void print_color_name(char number) {
@@ -45,25 +44,25 @@ void print_color_name(char number) {
 };
 
 // returns the color of the first sticker
-// index is a range from 0-7
-void get_color(long long face, short index) {
-  short shift_amount = 56 - (index * 8);
-  char color = (face >> shift_amount) & 0xff;
-  print_color_name(color);
-}
-
+// index is a range from [0, 48]
+void get_color(struct Cube *cube, int index) {
+  print_color_name(cube->white_face[index]);
+};
 
 int main() {
   struct Cube *cube = malloc(sizeof(struct Cube));
   // random face state
-  cube->white_face =
-      0x0203010004030500; // 0x 02 03 01 00 04 03 05 00 === G R O W __ B R Y W
-  get_color(cube->white_face, 0);
-  get_color(cube->white_face, 1);
-  get_color(cube->white_face, 2);
-  get_color(cube->white_face, 3);
-  get_color(cube->white_face, 4);
-  get_color(cube->white_face, 5);
-  get_color(cube->white_face, 6);
-  get_color(cube->white_face, 7);
+  // cube->white_face =
+  //     0x0203010004030500; // 0x 02 03 01 00 04 03 05 00 === G R O W __ B R Y W
+  cube->white_face[0] = G;
+  cube->white_face[1] = R;
+  cube->white_face[2] = O;
+  cube->white_face[3] = W;
+  cube->white_face[4] = B;
+  cube->white_face[5] = R;
+  cube->white_face[6] = Y;
+  cube->white_face[7] = W;
+  get_color(cube, 0);
+
+  free(cube);
 };
