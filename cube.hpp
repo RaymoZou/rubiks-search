8e49ea1a95ccf2acc923560a87a713538a8cf4d6
Raymond <raymond.1zou@gmail.com>
2024-06-19 22:10:29 -0700

    add cube header file

---
+++ cube.hpp
@@ -0,0 +1,12 @@
+class Cube {
+public:
+  char white_face[8];
+  char orange_face[8];
+  char green_face[8];
+  char red_face[8];
+  char blue_face[8];
+  char yellow_face[8];
+
+  Cube(char white[8], char orange[8], char green[8], char red[8], char blue[8],
+       char yellow[8]);
+};
